3c72f35038f20e16057fd7918c08b1e98120f96f
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-08-11 22:48:19 +0300

    dynamic props

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,15 +1,17 @@
 import "./ProductItem.css";
-function ProductItem(props) {
+function ProductItem({product}) {
+    const {imageUrl,productName,productPrice}=product;
   return (
     <div className="product-item">
       <div className="product-image">
-        <img src={props.imageUrl}></img>
+        <img src={imageUrl}></img>
       </div>
       <div className="product-info">
-        <h2>{props.productName}</h2>
-        <span>{props.productPrice} TL</span>
+        <h2>{productName}</h2>
+        <span>{productPrice} TL</span>
       </div>
     </div>
   ); //html içinde js kullanacağın zaman {} kullan
 }
 export default ProductItem;
+
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,24 +1,35 @@
 import ProductItem from "./ProductItem";
 import "./Products.css";
+
+const productData = [
+  {
+    imageUrl: "https://cdn.akakce.com/grundig/grundig-pc-2560-b1-i7-1-tb-z.jpg",
+    productName: "Grundig",
+    productPrice: 40000,
+  },
+  {
+    imageUrl:
+      "https://www.atombilisim.com.tr/Data/EditorFiles/blog/12-05-2023/masaustu-bilgisayar/bilgisayar-fiyatlari-masaustu.jpg",
+    productName: "Oyuncu Bilgisayarı",
+    productPrice: 60000,
+  },
+  {
+    imageUrl:
+      "https://cdn.vatanbilgisayar.com/Upload/PRODUCT/msi/thumb/138089-1_small.jpg",
+    productName: "MSI",
+    productPrice: 50000,
+  },
+];
 function Products() {
-  const imageUrl =
-    "https://cdn.akakce.com/grundig/grundig-pc-2560-b1-i7-1-tb-z.jpg";
-  const productName = "Grundig";
-  const productPrice = 40000;
   return (
     <div className="product-wrapper">
       <h1>Products</h1>
       <div className="products">
-        <ProductItem
-          imageUrl={imageUrl}
-          productName={productName}
-          productPrice={productPrice}
-        />
-        <ProductItem
-          imageUrl="https://www.atombilisim.com.tr/Data/EditorFiles/blog/12-05-2023/masaustu-bilgisayar/bilgisayar-fiyatlari-masaustu.jpg"
-          productName="Oyuncu Bilgisayarı"
-          productPrice="60000"
-        />
+        {productData.map((product) => {
+          return <ProductItem 
+          key={product.productName} 
+          product={product} />;
+        })}
       </div>
     </div>
   );
