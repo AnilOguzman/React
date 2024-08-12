3bcbae610df9a0003e911d5e4b65518380e83fc6
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-08-12 13:31:17 +0300

    useState hook

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,16 +1,26 @@
+import {useState} from "react"
 import "./ProductItem.css";
 import ProductInfo from "./ProductInfo";
 
 const ProductItem = ({ product }) => {
   const { imageUrl, productName, productPrice } = product;
+  const [title,setTitle] = useState(productName);
+  const clickHandler = () =>
+    {
+      setTitle("Güncellendi");
+      console.log(productName, "Güncellendi"); //id kullanmadan hangi kartın güncellendiğini state hooku sayesinde anladı
+    };
+
   return (
     <div className="product-item">
       <div className="product-image">
         <img src={imageUrl}></img>
       </div>
       <ProductInfo>
-        <h2>{productName}</h2>
+        <h2>{title}</h2>
         <span>{productPrice} TL</span>
+        <br />
+        <button onClick={clickHandler}>Sepete Ekle</button>
       </ProductInfo>
     </div>
   ); //html içinde js kullanacağın zaman {} kullan
