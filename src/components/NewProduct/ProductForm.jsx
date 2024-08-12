d5d5248c28d7be87fe0a8f20649dbc16f24aae30
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-08-12 15:40:47 +0300

    different way for multiple useState

--- src/components/NewProduct/ProductForm.jsx
+++ src/components/NewProduct/ProductForm.jsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
 import "./ProductForm.css";
 const ProductForm = () => {
-  const [productName, setProductName] = useState("");
-  const [productPrice, setProductPrice] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [productData, setProductData] = useState({
+    productName: "",
+    productPrice: "",
+    imageUrl: "",
+  });
   const titleChangeHandler = (event) => {
-    setProductName(event.target.value);
+    setProductData({
+      ...productData,
+      productName: event.target.value,
+    });
   };
   const priceChangeHandler = (event) => {
-    setProductPrice(event.target.value);
+    setProductData({
+      ...productData,
+      productPrice: event.target.value,
+    });
   };
   const imageChangeHandler = (event) => {
-    setImageUrl(event.target.value);
+    setProductData({
+      ...productData,
+      imageUrl: event.target.value,
+    });
   };
 
   return (
     <form className="product-form">
       <div className="product-form-input">
-        <label>Ürün Adı : {productName}</label>
+        <label>Ürün Adı </label>
         <input
           type="text"
           placeholder="Ürün Adı Giriniz..."
@@ -25,7 +36,7 @@ const ProductForm = () => {
         />
       </div>
       <div className="product-form-input">
-        <label>Ürün Fiyatı : {productPrice}</label>
+        <label>Ürün Fiyatı </label>
         <input
           type="number"
           placeholder="Ürün Fiyatı Giriniz..."
@@ -33,7 +44,7 @@ const ProductForm = () => {
         />
       </div>
       <div className="product-form-input">
-        <label>Ürün Görseli : {imageUrl}</label>
+        <label>Ürün Görseli </label>
         <input
           type="text"
           placeholder="Ürün Görseli Giriniz..."
