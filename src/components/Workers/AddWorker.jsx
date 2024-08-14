6367e0b47a040608663b63753e46041d6e2327a2
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-08-14 16:08:49 +0300

    Card component

--- src/components/Workers/AddWorker.jsx
+++ src/components/Workers/AddWorker.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-
+import Card from '../UI/Card'
 const AddWorker = () => {
   return (
-    <form className='flex flex-col gap-y-2'>
+    //css'i props olarak geçirdik dikkat et
+    <Card addClass="mt-10 "> 
+      <form className='flex flex-col gap-y-2'>
         <label htmlFor='name' className='font-medium'>Çalışan İsmi</label>
         <input type='text' className='max-w-[40rem] w-full mx-auto border p-2' placeholder='Çalışan İsmi yazınız' id='name'/>
         
         <label htmlFor='wage' className='font-medium'>Maaş Miktarı</label>
-        <input type='text' className='max-w-[40rem] w-full mx-auto border p-2' placeholder='Maaş Miktarı yazınız' id='wage'/>
+        <input type='number' className='max-w-[40rem] w-full mx-auto border p-2' placeholder='Maaş Miktarı yazınız' id='wage'/>
         
         <button className='p-2 text-lg bg-teal-700 text-white mt-2 '>Ekle</button>
+        
     </form>
+    </Card>
+  //butonları component halinde oluşturuyoruz çünkü bir buton var biz buna prop geçirerek primary dashed şekilleri verebiliyoruz
+  //yani aslında tek buton var ve biz buna prop vererek şekillendiriyoruz    
   )
 }
 
