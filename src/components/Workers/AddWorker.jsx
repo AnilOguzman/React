c1714f5c2946ebe09667c612154f19b033633b0f
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-08-14 19:48:25 +0300

    ErrorModal

--- src/components/Workers/AddWorker.jsx
+++ src/components/Workers/AddWorker.jsx
@@ -1,69 +1,96 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 const AddWorker = (props) => {
   const [enteredWorkerName, setEnteredWorkerName] = useState("");
   const [enteredWage, setEnteredWage] = useState("");
-  const minimumWage=5000;
-
+  const [error,setError]=useState(); 
+  const minimumWage = 5000;
 
   const workerNameChangeHandler = (e) => {
     setEnteredWorkerName(e.target.value);
-  }
+  };
   const wageChangeHandler = (e) => {
     setEnteredWage(e.target.value);
-  }
+  };
   const addWorkerHandler = (e) => {
     e.preventDefault();
-    if(enteredWorkerName.trim().length===0 || enteredWage.length===0){
+    if (enteredWorkerName.trim().length === 0  ) {
+      setError({
+        title:"İsim Alanı Zorunludur!",
+        message:"Lütfen bir isim giriniz."
+      })
       return;
     }
-    if(enteredWage<minimumWage){
+    if(enteredWage.length === 0){
+      setError({
+        title:"Maaş Alanı Zorunludur!",
+        message:"Lütfen maaşınızı giriniz."
+      })
       return;
     }
-    props.setWorkers((prevState)=>[
+    if (enteredWage < minimumWage) {
+      setError({
+        title:"Maaş Alanı Zorunludur!",
+        message:`Lütfen ${minimumWage} değerinden büyük bir maaş değeri giriniz.`
+      })
+      return;
+    }
+    props.setWorkers((prevState) => [
       {
-        id:Math.floor(Math.random()*1000),
-        name:enteredWorkerName,
-        wage:enteredWage 
+        id: Math.floor(Math.random() * 1000),
+        name: enteredWorkerName,
+        wage: enteredWage,
       },
-      ...prevState
+      ...prevState,
     ]);
-    setEnteredWorkerName("")
-    setEnteredWage("")
+    setEnteredWorkerName("");
+    setEnteredWage("");
+  };
+  const errorHandler = () => {
+    setError(null);
   }
   return (
+    <div>
+      {error && <ErrorModal onConfirm={errorHandler} error={error} />}
+      
+      
+      <Card addClass="mt-10 ">
+        <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
+          <label htmlFor="name" className="font-medium">
+            Çalışan İsmi
+          </label>
+          <input
+            type="text"
+            className="max-w-[40rem] w-full mx-auto border p-2"
+            placeholder="Çalışan İsmi yazınız"
+            id="name"
+            onChange={workerNameChangeHandler}
+            value={enteredWorkerName}
+          />
+
+          <label htmlFor="wage" className="font-medium">
+            Maaş Miktarı
+          </label>
+          <input
+            type="number"
+            className="max-w-[40rem] w-full mx-auto border p-2"
+            placeholder="Maaş Miktarı yazınız"
+            id="wage"
+            onChange={wageChangeHandler}
+            value={enteredWage}
+          />
+
+          <Button className="mt-2" type="submit">
+            Ekle
+          </Button>
+        </form>
+      </Card>
+      
+    </div>
     //css'i props olarak geçirdik dikkat et
-    <Card addClass="mt-10 ">
-      <form className="flex flex-col gap-y-2" onSubmit={addWorkerHandler}>
-        <label htmlFor="name" className="font-medium">
-          Çalışan İsmi
-        </label>
-        <input
-          type="text"
-          className="max-w-[40rem] w-full mx-auto border p-2"
-          placeholder="Çalışan İsmi yazınız"
-          id="name"
-          onChange={workerNameChangeHandler}
-          value={enteredWorkerName}
-        />
-
-        <label htmlFor="wage" className="font-medium">
-          Maaş Miktarı
-        </label>
-        <input
-          type="number"
-          className="max-w-[40rem] w-full mx-auto border p-2"
-          placeholder="Maaş Miktarı yazınız"
-          id="wage"
-          onChange={wageChangeHandler}
-          value={enteredWage}
-        />
-
-        <Button className="mt-2" type="submit">Ekle</Button>
-      </form>
-    </Card>
-    //butonları component halinde oluşturuyoruz çünkü bir buton var biz buna prop geçirerek primary dashed şekilleri verebiliyoruz
+    //butonları component halinde oluşturuyoruz çünkü bir buton var biz buna prop geçirerek primary dashed şekilleri verebiliyoruz 
     //yani aslında tek buton var ve biz buna prop vererek şekillendiriyoruz
   );
 };
