6e3217fcb4132ab7d2bc6c2dc74f7d22f949ecba
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-08-26 22:24:22 +0300

    my first multiple page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/Home";
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
