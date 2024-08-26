b0b54f827008a92c1621b47b8fd7ba7dbb1a7ab5
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-08-26 22:48:25 +0300

    second way to route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import "./App.css";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import "./App.css";
 
-const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+// const router = createBrowserRouter([
+//   { path: "/", element: <HomePage /> },
+//   { path: "/products", element: <ProductsPage /> },
+// ]);
 
+const routeDefinitions=createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage/>} />
+    <Route path="/products" element={<ProductsPage/>} />
+  </Route>
+);
+const router=createBrowserRouter(routeDefinitions);
 function App() {
   return (
     <>
