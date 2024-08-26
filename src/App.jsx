bef421c35a9d1f64d488c37b83c096f2ac9f4cbd
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-08-26 23:28:59 +0300

    link operation

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import "./App.css";
 
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductsPage /> },
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
-const routeDefinitions=createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage/>} />
-    <Route path="/products" element={<ProductsPage/>} />
-  </Route>
-);
-const router=createBrowserRouter(routeDefinitions);
+// const routeDefinitions=createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage/>} />
+//     <Route path="/products" element={<ProductsPage/>} />
+//   </Route>
+// );
+// const router=createBrowserRouter(routeDefinitions);
 function App() {
   return (
     <>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 
 const HomePage = () => {
   return (
-    <h1>My Home Page</h1>
-  )
-}
+    <>
+      <h1>My Home Page</h1>
+      <Link to="/products">Go to Products Page</Link>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
