e3f28dc0754f0fd008e3062ce3d41511bcd6819e
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-08-26 14:57:13 +0300

    multiple slice

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+      <header>
+        <h1>Header</h1>
+        <button>Logout</button>
+      </header>
+    );
+  };
+  
+  export default Header;
+  
--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,49 @@
 import { createSlice,configureStore } from "@reduxjs/toolkit";
 
-const initialState = {
-  counter: 2,
+const initialCounterState = {
+  value: 2,
   showCounter: true,
 };
 
 const counterSlice=createSlice({
   name:"counter", // yapacağım işlemin neyle ilgili olduğu unque olmalı
-  initialState,  //stateleri verdik 
+  initialState:initialCounterState,  //stateleri verdik 
   reducers:{
     increment(state){    //burada reducer fonksiyonlarımızı yazıyoruz state dediğimiz aslında initialState.
-      state.counter++   //burada ...state yapmadık çünkü zaten kendi tüm stateleri otomatik alıyor
+      state.value++   //burada ...state yapmadık çünkü zaten kendi tüm stateleri otomatik alıyor
     },
     increase(state,action){
-      state.counter+=action.payload; //birden fazla veri geliyosa örneğin amount geldi action.payload.amount ile ulaş
+      state.value+=action.payload; //birden fazla veri geliyosa örneğin amount geldi action.payload.amount ile ulaş
     },
     decrement(state){
-      state.counter--;
+      state.value--;
     },
     toggleCounter(state){
       state.showCounter=!state.showCounter;
     }
   }
 });
-
+const initialAuthState={
+  isAuthenticated:false,
+}
+const authSlice=createSlice({
+name:"authentication",
+initialState:initialAuthState,
+reducers:{
+  login(state){
+    state.isAuthenticated=true;
+  },
+  logout(state){
+    state.isAuthenticated=false;
+  }
+}
+});
 
 
 const store = configureStore({
-  reducer:counterSlice.reducer  //counterSlice içindeki reducerları dışarı aktardık
+  reducer:{counter:counterSlice.reducer,auth:authSlice.reducer}  //counterSlice içindeki reducerları dışarı aktardık
 });
 
 export const counterActions=counterSlice.actions;  
-
+export const authActions=authSlice.actions;
 export default store;
