8ef22a801d08833511e380ddd9984aa7f8945fec
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-08-27 08:55:57 +0300

    navigation onClick any elements by useNavigation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate,  } from "react-router-dom";
 
 
 const HomePage = () => {
+    const navigate=useNavigate();
+
+    const navigateHandler = () =>{
+        navigate("/products");
+    }
   return (
     <>
 
       <h1>My Home Page</h1>
       <Link to="/products">Go to Products Page</Link>
+
+      <button onClick={navigateHandler}>Go to Products</button>
+      <span onClick={navigateHandler}>Navigation with span</span>
     </>
   );
 };
