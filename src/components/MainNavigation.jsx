ce2d329fa3617f5cb26e5bcf0a6a8298dbd8535a
Anıl Oğuzman <aniluguzman38@gmail.com>
2024-08-27 08:49:10 +0300

    NavLink

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,13 +1,13 @@
 //LAYOUT COMPONENT
 
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const MainNavigation = () => {
   return (
     <header>
         <nav>
             <ul>
-                <li><Link to="/">Home</Link></li>
+                <li><NavLink to="/" className={({isActive})=>(isActive ? "active" : undefined)}>Home</NavLink></li>
                 <li><Link to="/products">Products</Link></li>
             </ul>
         </nav>
